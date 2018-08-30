d459b085c6df3eb9d1cefcc218ce28ebe6c69958
Yanbin Lv <yan.bin@shannon-sys.com>
2018-08-30 17:21:31 +0800

    build data-link bewteen monitor & httpserver
    Signed-off-by: Yanbin Lv <yan.bin@shannon-sys.com>

--- src/views/console/content/status.vue
+++ src/views/console/content/status.vue
@@ -15,15 +15,15 @@
                 let ifbw = this.$echarts.init(document.getElementById('InterfaceBandwidth'));
 
                 ifbw.setOption({
-                    title: { text: 'ECharts 入门示例' },
+                    title: { text: 'Bandwidth' },
                     tooltip: {},
                     xAxis: {
-                            data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+                        data: ['text1', 'text2', 'text3', 'text4', 'text5', 'text6']
                     },
                     yAxis: {},
                     series: [{
-                        name: '销量',
-                        type: 'bar',
+                        name: 'bw',
+                        type: 'line',
                         data: [5, 20, 36, 10, 10, 20]
                     }]
                 });
--- superviser.py
+++ superviser.py
@@ -1,9 +1,11 @@
 #!/usr/local/bin/python3
 
 import threading, time, io, json, sys, os
+import socketserver
 
 from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
 from http import HTTPStatus
+from functools import partial
 
 ERROR_DATA = """\
 {
@@ -19,25 +21,54 @@ class Monitor():
         self.should_stop = False
         self.extractor = threading.Thread(target=self.extract, name="extractor")
         self.net_file = open('/proc/net/dev', "r")
+        self.net_recv_bw_data1 = 0
+        self.net_recv_bw_data2 = 0
+        self.net_send_bw_data1 = 0
+        self.net_send_bw_data2 = 0
+        self.bw_buffer_len = 2 * 4
+        self.recv_bw_buffer = [0] * self.bw_buffer_len
+        self.send_bw_buffer = [0] * self.bw_buffer_len
+        self.bw_buffer_p = 0
+        self.tag = 'this is monitor tag'
 
     def __del__(self):
         self.net_file.close()
 
     def extract(self):
         print('extracting started.')
-        while not self.should_stop:
-            self.net_file.seek(0)
-            for line in self.net_file:
-                if (len(line.split(':')) != 2):
-                    continue
-                if ('wlp3s0' in line):
-                    l = line.replace(':', ' ').split()
-                    if len(l) < 17: continue
-                    if l[2] == '0' and l[10] == '0': continue
-                    name = l[0]
-                    print('%s' % l[0])
-                    print('%s' % line, end='')
-            time.sleep(1)
+        try:
+            while not self.should_stop:
+                self.net_file.seek(0)
+                recv_bw = 0
+                send_bw = 0
+                for line in self.net_file:
+                    if (len(line.split(':')) != 2):
+                        continue
+                    if ('eth0' in line):
+                        l = line.replace(':', ' ').split()
+                        if (len(l) < 17):
+                            continue
+                        if (l[2] == '0' and l[10] == '0'):
+                            continue
+
+                        self.net_recv_bw_data2 = int(l[1])
+                        if (self.net_recv_bw_data1 != 0):
+                            recv_bw = self.net_recv_bw_data2 - self.net_recv_bw_data1
+                        self.net_recv_bw_data1 = self.net_recv_bw_data2
+
+                        self.net_send_bw_data2 = int(l[9])
+                        if (self.net_send_bw_data1 != 0):
+                            send_bw = self.net_send_bw_data2 - self.net_send_bw_data1
+                        self.net_send_bw_data1 = self.net_send_bw_data2
+
+                        self.recv_bw_buffer[self.bw_buffer_p] = recv_bw
+                        self.send_bw_buffer[self.bw_buffer_p] = send_bw
+                        self.bw_buffer_p = (self.bw_buffer_p + 1) % self.bw_buffer_len
+
+                        print('recv: %d, send: %d' % (recv_bw, send_bw))
+                time.sleep(1)
+        except KeyboardInterrupt:
+            pass
 
     def dispatch(self):
         self.extractor.start()
@@ -47,23 +78,59 @@ class Monitor():
         self.extractor.join()
         print('extracting stoped.')
 
+    def pull_data(self, request):
+        print('type: %s, length: %d' % (request['type'], request['length']))
+        req_type = request['type']
+        if (req_type == 'net_bw'):
+            length = request['length']
+            if (length > self.bw_buffer_len):
+                length = self.bw_buffer_len
+            position = self.bw_buffer_p - 1
+            recv_bw = []
+            send_bw = []
+            if (position >= (length - 1)):
+                recv_bw.extend(self.recv_bw_buffer[(position - length + 1) : (position + 1)])
+                send_bw.extend(self.send_bw_buffer[(position - length + 1) : (position + 1)])
+            else:
+                recv_bw.extend(self.recv_bw_buffer[(self.bw_buffer_len - (length - position) + 1):])
+                recv_bw.extend(self.recv_bw_buffer[:(position + 1)])
+                send_bw.extend(self.send_bw_buffer[(self.bw_buffer_len - (length - position) + 1):])
+                send_bw.extend(self.send_bw_buffer[:(position + 1)])
+            net_bw = { 'recv': recv_bw, 'send': send_bw }
+            return net_bw
+        else:
+            return {}
+
 
 class Local_HTTP_Request_Handler(BaseHTTPRequestHandler):
-    error_message_format = ERROR_DATA
-    error_content_type = ERROR_TYPE
-    server_version = "Supervisor/1.0"
-    sys_version = ""
 
-    error_list = [
-        { 'code': 0, 'msg': 'placeholder' },
-        { 'code': 1, 'msg': 'Bad JSON format' },
-        { 'code': 2, 'msg': 'Need item "type" in request data' },
-        { 'code': 3, 'msg': 'Unknow type data' }
-    ]
+    def __init__(self, *args, mon=None, **kwargs):
+        self.monitor = mon
+        self.error_message_format = ERROR_DATA
+        self.error_content_type = ERROR_TYPE
+        self.server_version = "Supervisor/1.0"
+        self.sys_version = ""
+        self.error_list = [
+            { 'code': 0, 'msg': 'placeholder' },
+            { 'code': 1, 'msg': 'Bad JSON format' },
+            { 'code': 2, 'msg': 'Need item "type" in request data' },
+            { 'code': 3, 'msg': 'Unknow type data' },
+            { 'code': 4, 'msg': 'Need item "length" in request data' }
+        ]
+        super().__init__(*args, **kwargs)
+
+    def make_error_response(self, code):
+        return (self.error_message_format % {
+            'code': self.error_list[code]['code'],
+            'message': self.error_list[code]['msg']
+        })
 
     def handle_ping(self):
         response = { 'status': 'OK', 'data': 'pong' }
         return response
+
+    def handle_monitor_req(self, req):
+        return self.monitor.pull_data(req)
 
     def do_POST(self):
         obj1 = {}
@@ -76,31 +143,29 @@ class Local_HTTP_Request_Handler(BaseHTTPRequestHandler):
 
         response_status = HTTPStatus.OK
         response_str = ''
-        request_data = self.rfile.read1()
 
         try:
+            request_data = self.rfile.read1()
             request_obj = json.loads(request_data)
             if (request_obj.__contains__('type')):
-                if (request_obj['type'] == 'ping'):
+                req_type = request_obj['type']
+                if (req_type == 'ping'):
                     response_str = json.dumps(self.handle_ping())
+                elif (req_type == 'net_bw'):
+                    if (request_obj.__contains__('length')):
+                        response_str = json.dumps(self.handle_monitor_req( { 'type':'net_bw', 'length':request_obj['length'] } ))
+                    else:
+                        response_status = HTTPStatus.BAD_REQUEST
+                        response_str = self.make_error_response(4)
                 else:
                     response_status = HTTPStatus.BAD_REQUEST
-                    response_str = (self.error_message_format % {
-                        'code': self.error_list[3]['code'],
-                        'message': self.error_list[3]['msg']
-                    })
+                    response_str = self.make_error_response(3)
             else:
                 response_status = HTTPStatus.BAD_REQUEST
-                response_str = (self.error_message_format % {
-                    'code': self.error_list[2]['code'],
-                    'message': self.error_list[2]['msg']
-                })
+                response_str = self.make_error_response(2)
         except json.JSONDecodeError as msg:
             response_status = HTTPStatus.BAD_REQUEST
-            response_str = (self.error_message_format % {
-                'code': self.error_list[1]['code'],
-                'message': self.error_list[1]['msg']
-            })
+            response_str = self.make_error_response(1)
 
         self.close_connection = True
         self.protocol_version = 'HTTP/1.1'
@@ -119,7 +184,11 @@ class Local_HTTP_Request_Handler(BaseHTTPRequestHandler):
         #print('protocol_version: %s' % self.protocol_version)
 
 class MainHTTPServer(ThreadingHTTPServer):
+    def set_monitor(self, mon):
+        self.monitor = mon
+
     def server_bind(self):
+        socketserver.TCPServer.server_bind(self)
         host, port = self.server_address[:2]
         self.server_name = host if (host != '') else os.popen('hostname').read()
         self.server_port = port
@@ -129,7 +198,8 @@ def main(server_class=MainHTTPServer, handler_class=Local_HTTP_Request_Handler):
     monitor.dispatch()
 
     server_address = ('', 8000)
-    http_server = server_class(server_address, handler_class)
+    http_server = server_class(server_address, partial(handler_class, mon=monitor))
+    http_server.set_monitor(monitor)
     try:
         http_server.serve_forever()
     except KeyboardInterrupt:
