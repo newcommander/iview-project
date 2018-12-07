3500d21ce637769679af6ccaad80f26a4cc4f44b
new_commander <new_commander@163.com>
2018-12-07 23:53:02 +0800

    Add link_list chart.
    Signed-off-by: new_commander <new_commander@163.com>

--- src/views/console/content/status.vue
+++ src/views/console/content/status.vue
@@ -5,6 +5,7 @@
         <Table stripe no-data-text="none" no-filtered-data-text="none" :columns="clients_list_header" :data="clients_list"></Table>
         <Table stripe no-data-text="none" no-filtered-data-text="none" :columns="if_list_header" :data="if_list"></Table>
         <Table stripe no-data-text="none" no-filtered-data-text="none" :columns="route_table_header" :data="route_table"></Table>
+        <div id="Linklist" style="width: 600px;height:250px;"></div>
     </div>
 </template>
 <script>
@@ -45,16 +46,20 @@
                     { title: 'Gateway', key: 'gateway' },
                     { title: 'Interface', key: 'interface' }
                 ],
-                route_table: []
+                route_table: [],
+                link_list: null
             }
         },
         mounted: function () {
             let if_bw = this.$echarts.init(document.getElementById('InterfaceBandwidth'));
+            let link_list = this.$echarts.init(document.getElementById('Linklist'));
             this.if_bw = if_bw
+            this.link_list = link_list
             this.init_if_bw_chart()
             this.update_clients_list()
             this.update_interface_list()
             this.update_route_table()
+            this.init_link_list_chart()
         },
         methods: {
             is_object_empty(obj) {
@@ -217,6 +222,175 @@
                     // something error.
                 })
                 setTimeout(() => { this.update_if_bw_chart() }, 1000)
+            },
+            init_link_list_chart () {
+                let option = {
+                    animation: false,
+                    title: {
+                        text: "Link list",
+                        x: "center"
+                    },
+                    tooltip: {
+                        trigger: "axis",
+                        axisPointer: {
+                            type: "shadow",
+                            areaStyle: {
+                                color: "rgb(114, 182, 249)"
+                            }
+                        }
+                    },
+                    legend: {
+                        data: ["in", "out"],
+                        orient: "vertical",
+                        x: "right"
+                    },
+                    toolbox: {
+                        show: false
+                    },
+                    calculable: true,
+                    xAxis: [
+                        {
+                            type: "value",
+                            position: "top",
+                            name: "KB/s",
+                            nameLocation: "end",
+                            nameTextStyle: {
+                                fontWeight: "bolder"
+                            },
+                            min: 0,
+                            axisLine: {
+                                show: false
+                            },
+                            axisLabel: {
+                                otate: 0,
+                                show: true
+                            },
+                            splitNumber: 5,
+                            axisTick: {
+                                show: true,
+                                lineStyle: {
+                                    color: "rgb(186, 186, 186)"
+                                }
+                            },
+                            splitLine: {
+                                show: true,
+                                lineStyle: {
+                                    type: "dashed",
+                                    color: "rgb(124, 124, 124)"
+                                }
+                            },
+                            splitArea: {
+                                show: false
+                            }
+                        }
+                    ],
+                    yAxis: [
+                        {
+                            type: "category",
+                            data: [],
+                            position: "left",
+                            name: "Links",
+                            nameLocation: "start",
+                            nameTextStyle: {
+                                color: "rgb(40, 40, 40)"
+                            },
+                            axisLine: {
+                                show: true,
+                                lineStyle: {
+                                    color: "rgb(110, 179, 229)"
+                                }
+                            },
+                            axisLabel: {
+                                textStyle: {
+                                    fontWeight: "bolder"
+                                }
+                            },
+                            axisTick: {
+                                show: false
+                            },
+                            splitArea: {
+                                show: true
+                            },
+                            splitLine: {
+                                show: false
+                            }
+                        }
+                    ],
+                    series: [
+                        {
+                            name: "in",
+                            type: "bar",
+                            barWidth: 7,
+                            data: []
+                        },
+                        {
+                            name: "out",
+                            type: "bar",
+                            barWidth: 7,
+                            data: []
+                        }
+                    ],
+                    grid: {
+                        x: 119
+                    }
+                }
+                this.link_list.setOption(option)
+                setTimeout(() => { this.update_link_list_chart() }, 10)
+            },
+            update_link_list_chart () {
+                let link_list = []
+                let link_in = []
+                let link_out = []
+                let max = 0
+                let max_x = 0
+                let grid_height = 300
+                this.$http.post('/ifmonitor', '{"if_name":"tun0","item":"link_list"}').then(function (response) {
+                    grid_height = 20 * response.data.data.links.length
+                    for (let i = response.data.data.links.length - 1; i >= 0; i--) {
+                        let s_ip = response.data.data.links[i].s_ip
+                        let d_ip = response.data.data.links[i].d_ip
+                        let in_bytes = response.data.data.links[i].s2d_len
+                        let out_bytes = response.data.data.links[i].d2s_len
+                        link_list.push(s_ip + ' > ' + d_ip)
+                        link_in.push((in_bytes / 1024).toFixed(2))
+                        link_out.push((out_bytes / 1024).toFixed(2))
+                        if ((in_bytes / 1024) > max)
+                            max = in_bytes / 1024
+                        if ((out_bytes / 1024) > max)
+                            max = out_bytes / 1024
+                    }
+                    if (max < 40) {
+                        max_x = 50
+                    } else if (max < 80) {
+                        max_x = 100
+                    } else if (max < 400) {
+                        max_x = 500
+                    } else if (max < 800) {
+                        max_x = 1000
+                    } else if (max < 1600) {
+                        max_x = 2000
+                    } else {
+                        max_x = 4000
+                    }
+                    this.link_list.setOption({
+                        grid: {
+                            height: grid_height
+                        },
+                        xAxis: [
+                            { max: max_x }
+                        ],
+                        yAxis: [
+                            { data: link_list }
+                        ],
+                        series: [
+                            { data: link_in },
+                            { data: link_out }
+                        ]
+                    })
+                }, function (response) {
+                    // something error.
+                })
+                setTimeout(() => { this.update_link_list_chart() }, 1000)
             }
         }
     }
